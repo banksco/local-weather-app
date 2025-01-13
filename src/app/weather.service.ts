bd76a35a48dbcb132c85e45e68c1a8c14bcbc794
banksco <cdbankxx@gmail.com>
2025-01-12 18:44:26 -0600

    Mod4

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { ICurrentWeatherData } from './icurrent-weather-data';
+import { IcurrentWeather } from './icurrent-weather';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,7 +11,19 @@ export class WeatherService {
   constructor(private httpClient: HttpClient  ) { }
 
     getCurrentWeather(city:string, country:string){
-      this.httpClient.get("https://api.openweathermap.org/data/2.5/weather?q="${city},${country}&appid={})
+      this.httpClient.get<ICurrentWeatherData>(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${environment.appId}`)
+    }
+
+    private transformToICurrentWeather(data: ICurrentWeatherData): IcurrentWeather{
+      return {
+        city: data.name,
+        country: data.sys.country,
+        date: new Date(data.dt * 1000),
+        temperature: data.main.temp * 9/5 - 459.67,
+        description: data.weather[0].description,
+        image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
+      }
+
     }
 }
  
